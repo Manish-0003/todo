bcf6cfddfb3a177dd1adbc6defa4cfd91c3d17d9
Manish-0003 <manish.sharma9314@gmail.com>
2024-05-25 23:47:42 +0530

    task 13

--- pages/index.js
+++ pages/index.js
@@ -27,9 +27,9 @@ export default function Home() {
     }
    }
 
-   const onChange = (e) => { 
+   const onChange = (event) => { 
 
-      setTodo({...todo, [e.target.name]: e.target.value})
+      setTodo({...todo, [event.target.name]: event.target.value})
       console.log(todo)
     }
   return (
@@ -48,7 +48,7 @@ export default function Home() {
               <input onChange={onChange} value={todo.desc} type="text" id="desc" name="desc" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
             </div>
             <button onClick={addTodo} class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none w-fit hover:bg-indigo-600 rounded text-lg">Add Todo</button>
-            <p class="text-xs text-gray-500 mt-3">Literally you probably haven't heard of them jean shorts.</p>
+           
           </div>
         </div>
       </section>
